d8b962d1f51d1df8f1ade6a913379cf16d7c8f76
Chandru Rajendran <51230988+chandru-199@users.noreply.github.com>
2021-01-11 09:23:26 +0530

    Add files via upload

---
+++ number-pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, val;
+    cout<<"enter number"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++){
+            val = i<j?i:j;
+            cout<<n-val+1;
+        }
+        cout<<endl;
+    }
+}
